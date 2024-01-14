48358c6b00b59f10eb395bd159c47c64b17acabb
Kiran-enmovil <156183308+Kiran-enmovil@users.noreply.github.com>
2024-01-14 13:49:35 +0530

    ag-grid first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useMemo, useEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css'; // Core CSS
+import 'ag-grid-community/styles/ag-theme-quartz.css'; // Theme
 
-function App() {
+
+function App()  {
+    const celClickedListener = (e) => {
+        console.log(e)
+    }
+
+       const [rowData, setRowData] = useState([
+        {make:'Ford' , model : 'focus', price: 40000},
+        {make:'Toyota' , model : 'Calica', price: 45000},
+        {make:'BMW' , model : '4 Series', price: 50000},
+    ]);
+
+    const [colDefs, setcolDefs]= useState([
+        {field : 'make'},
+        {field : 'model'},
+        {field : 'price'},
+    ]);
+
+    const defaultColDef = useMemo(()=> ({
+        sortable :true,
+        filter : true
+    }), []);
+
+    useEffect(()=> {
+        fetch("https://www.ag-grid.com/example-assets/row-data.json")
+        .then(response => response.json())
+        .then(data => setRowData(data))
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="ag-theme-quartz-dark" style={{height:500}}
+    >
+      <AgGridReact
+      onCellClicked={celClickedListener} rowData={rowData} columnDefs={colDefs} defaultColDef={defaultColDef} rowSelection='multiple' animateRows={true}/>
     </div>
   );
-}
+};
 
-export default App;
+export default App
